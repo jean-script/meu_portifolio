35620057dffc289c0ad1b9951515bf380f28337e
Jean_Carlos <79288480+jean-script@users.noreply.github.com>
2023-06-07 18:21:11 -0300

    add footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,24 @@
+import { AiOutlineInstagram, AiFillLinkedin } from 'react-icons/ai'
+import { TfiEmail } from 'react-icons/tfi'
+
 import styles from './styles.module.css'
 
 export default function Footer(){
     return (
         <footer className={styles.container}>
+            
+            <div className={styles.links}>
+                {/* <a href='#'  rel='noreferrer' target='_blank'>
+                    <AiOutlineInstagram size={50} color='#fff' />
+                </a>
+                <a href='https://www.linkedin.com/in/jean-carlos-aires/' rel='noreferrer' target='_blank'>
+                    <AiFillLinkedin size={50} color='#fff' />
+                </a>
+                <a href='mailto:jean.aires21@gmail.com?subject=Sobre o portifolio' rel='noreferrer' target='_blank'>
+                    <TfiEmail size={50} color='#fff' />
+                </a> */}
+            
+            </div>
         </footer> 
     )
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,23 +1,24 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 
 import Sobre from "../../components/Sobre";
 import Projetos from '../../components/Projetos';
 import Competencia from "../../components/Competencias";
 
-
+import { ProjetosContext } from '../../contexts/Projects'
 
 import styles from './styles.module.css';
 
 function Home(){
 
-    let sobreTexto = 'Olá, meu nome é Jean Carlos, tenho 22 anos e tenho 2 anos de experiência na área da programação WEB e RPA. Atualmente me formei em Análise e Desenvolvimento de Sistemas pela Universidade Nove de Julho, e estou atuando como Desenvolvedor RPA na empresa Practia Brasil, onde desenvolvi projetos internos de média e alta complexidade, apliquei treinamentos para outras empresas, atuei em clientes externos no modelo Outsourcing e ajudei na criação, juntamente a uma equipe sinérgica e envolvida, de um Framework em Python. Hoje, tenho domínio das tecnologias HTML, CSS, JavaScript, Python, Reactjs e algumas plataformas de RPA, nas quais desenvolvi alguns projetos. Meu próximo objetivo é atuar como desenvolvedor front end com intuito de adquirir novos conhecimentos e vivencias além de torná-los sólidos. Entre minhas características pessoais estão a competência em resolução de problemas, dedicação, boa comunicação, facilidade no aprendizado, proativo, excelente relacionamento interpessoal, bom humor e responsabilidade.'
 
-    const [name] = useState('Jean Carlos Aires');
-    const [cargo] = useState('Desenvolvedor RPA');
-    const [empresa] = useState('Practia Brasil');
-    const [faculdade] = useState('Analise e desenvolvimento de sistemas');
-    const [universidade] = useState('Uninove');
-    const [sobre] = useState(sobreTexto);
+    const { userInfo } = useContext(ProjetosContext);
+
+    const [name] = useState(userInfo.nome);
+    const [cargo] = useState(userInfo.cargo);
+    const [empresa] = useState(userInfo.empresa);
+    const [faculdade] = useState(userInfo.faculdade);
+    const [universidade] = useState(userInfo.universidade);
+    const [sobre] = useState(userInfo.sobre);
     const [id]= useState('');
 
     return(
@@ -42,7 +43,6 @@ function Home(){
             </div>
 
            
-            
         </main>
     )
 }
